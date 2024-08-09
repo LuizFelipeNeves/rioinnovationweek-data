7ab4976a7c0e0c862aa3c2a36471725531fcfc54
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2024-08-09 11:56:15 -0300

    Added day 14

--- buscar_dados.js
+++ buscar_dados.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 
 (async () => {
-    const dates = ['13', '14', '15', '16'].slice(0, 1)
+    const dates = ['13', '14', '15', '16']
 
     const output = dates.map(date => {
         const data = `2024-8-${date}`
@@ -11,15 +11,27 @@ const cheerio = require('cheerio');
         const divs = $('.MuiGrid-direction-xs-column > div').children().toArray().map(div => {
             const id = $(div).attr('id')
             const hora = id.startsWith('timeslotDivider') ? $(div).find('p').text() : null
-            const conferencia = $(div).find('div > div > div > div > div.MuiBox-root.jss77 > span > span').text()
+            let conferencia = $(div).find('div > div > div > div > div.MuiBox-root.jss77 > span > span').text()
+            if(conferencia){
+                conferencia = conferencia.split('\n')[0]
+
+                if(conferencia.indexOf(' - ') === conferencia.length - 3){
+                    conferencia = conferencia.slice(0, -3)
+                }
+            }
+
             const durancao = $(div).find('div > div > div > div > div.MuiBox-root.jss77 > div > span > span').text().trim()
             const titulo = $(div).find('#Timeslot__title--opening-night').text()
-            const descricao = $(div).find('div > div > div > div > p').html()
+            let descricao = $(div).find('div > div > div > div > p').html()
+            if (descricao) {
+                descricao = descricao.split('&amp;').join('&')
+            }
+
             const palestrantes = $(div).find('div > div > div > div > div.MuiGrid-container > div.MuiGrid-container').toArray().map(palestranteItem => {
                 const foto = $(palestranteItem).find('img').attr('src')
                 const nome = $(palestranteItem).find('p').first().text()
                 const empresaCargo = $(palestranteItem).find('p span').last().html()
-                const [empresa, cargo] = empresaCargo.split('<br>').map(item => item.trim())
+                const [empresa, cargo] = empresaCargo.split('<br>').map(item => item.trim().split('&amp;').join('&'))
 
                 return {
                     foto,
